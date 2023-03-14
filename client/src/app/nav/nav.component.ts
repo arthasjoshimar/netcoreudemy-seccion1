dc640f576237ed68b01416e5cc396e5c3a85fc2c
DESARROLLADOR 1 WINDOWS <developerruat1@gmail.com>
2023-03-13 22:45:54 -0400

    End of section  6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,7 +14,9 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(protected accountService:AccountService) { }
+  constructor(protected accountService:AccountService, 
+              private router: Router,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -19,15 +24,14 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe(
      {
-      next: resp => {
-        console.log(resp); 
-      },
-      error: error=> console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),// el _ hace referencia a que no se usara el parametro response
+      error: error=> { this.toastr.error(error.error) }
      }
     );
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
